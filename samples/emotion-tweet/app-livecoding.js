e4f330f2d8b25e60d1206e0f7e64d0555b47b538
Hosung Kim <hs852.kim@samsung.com>
2017-11-21 16:52:45 +0900

    ADD GPIO code in app-livecoding

--- samples/emotion-tweet/app-livecoding.js
+++ samples/emotion-tweet/app-livecoding.js
@@ -2,12 +2,35 @@ var fs = require('fs');
 var http = require('http');
 var querystring = require('querystring');
 var url = require('url');
+var Gpio = require('gpio');
 
 var config = require('./config');
 var device = require('./device');
 var utils = require('./utils');
 
 var count = 0;
+var gpio = new Gpio();
+
+var RED_PIN = 0,
+      GREEN_PIN = 1,
+      BLUE_PIN = 2;
+
+var LED_ON = true,
+      LED_OFF = false;
+
+var ledPin = [31, 30, 32];
+var ledGpio = [];
+
+function createLedGpio() {
+  for (var i = 0; i < ledPin.length; i++) {
+    ledGpio[i] = gpio.open({
+      pin: ledPin[i],
+      direction: gpio.DIRECTION.OUT,
+    });
+  }
+}
+
+createLedGpio();
 
 var server = http.createServer(function(req, res) {
   var data = '';
@@ -42,7 +65,24 @@ var server = http.createServer(function(req, res) {
 
 function change_mood(score) {
   // TODO: change mood based on emotion score (-5 ~ 5)
+  if (ledGpio.length < ledPin.length) {
+    return;
+  }
+
   console.log(count++, 'TODO: change mood light based on emotion score:', score);
+  if (score < 0) {  // turn on blue led
+    ledGpio[BLUE_PIN].writeSync(LED_ON);
+    ledGpio[GREEN_PIN].writeSync(LED_OFF);
+    ledGpio[RED_PIN].writeSync(LED_OFF);
+  } else if (score === 0) { // turn on green led
+    ledGpio[GREEN_PIN].writeSync(LED_ON);
+    ledGpio[BLUE_PIN].writeSync(LED_OFF);
+    ledGpio[RED_PIN].writeSync(LED_OFF);
+  } else { // socre > 0, turn on red led
+    ledGpio[RED_PIN].writeSync(LED_ON);
+    ledGpio[GREEN_PIN].writeSync(LED_OFF);
+    ledGpio[BLUE_PIN].writeSync(LED_OFF);
+  }
 }
 
 function close_application(data) {
